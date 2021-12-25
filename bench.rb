64b178cd0b26aa2a79d291f1e42fabf40374906d
Rubin Simons <me@rubin55.org>
2021-12-25 17:16:44 +0100

    Some LSP + Rubocop fun.

--- bench.rb
+++ bench.rb
@@ -1,35 +1,40 @@
-require "benchmark"
-#require "benchmark/memory"
+# frozen_string_literal: true
 
-def sum(a)
-    a.reduce(:+)
+require 'benchmark'
+# require "benchmark/memory"
+
+def sum(input)
+  input.reduce(:+)
 end
 
-def my_xbonacci(signature, n)
-    count_from_index = 0
-    digits_to_sum = signature.length
-    until signature.length == n
-        next_num = sum(signature.slice(count_from_index, digits_to_sum))
-        signature.push(next_num)
-        count_from_index += 1
-    end
-    signature
+def my_xbonacci(signature, number)
+  count_from_index = 0
+  digits_to_sum = signature.length
+  until signature.length == number
+    next_num = sum(signature.slice(count_from_index, digits_to_sum))
+    signature.push(next_num)
+    count_from_index += 1
+  end
+  signature
 end
 
-def their_xbonacci(s, n)
-    n.times.map { s << s.inject(:+); s.shift }
+def their_xbonacci(signature, number)
+  number.times.map do
+    signature << signature.inject(:+)
+    signature.shift
+  end
 end
 
-def benchmark(signature, n)
-    Benchmark.bmbm do |x|
-        x.report("My xbonacci") { my_xbonacci(signature, n) }
-        x.report("Their xbonacci") { their_xbonacci(signature, n) }
-    end
+def benchmark(signature, number)
+  Benchmark.bmbm do |x|
+    x.report('My xbonacci') { my_xbonacci(signature, number) }
+    x.report('Their xbonacci') { their_xbonacci(signature, number) }
+  end
 
-    #Benchmark.memory do |x|
-    #   x.report("My xbonacci") {my_xbonacci(signature, n)}
-    #   x.report("Their xbonacci") {their_xbonacci(signature, n)}
-    #end
+  # Benchmark.memory do |x|
+  #   x.report("My xbonacci") {my_xbonacci(signature, n)}
+  #   x.report("Their xbonacci") {their_xbonacci(signature, n)}
+  # end
 end
 
 benchmark([1, 2, 3], 1000)
